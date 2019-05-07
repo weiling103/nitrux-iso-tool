412b5f280bb2e1bc7aa7c1aaa48126dcf89dc8f9
Uri Herrera <hello@uriherrera.com>
2019-05-06 19:00:26 -0500

    test build

--- bootstrap.sh
+++ bootstrap.sh
@@ -39,7 +39,7 @@ cp /configs/sources.list /etc/apt/sources.list
 
 apt -qq update > /dev/null
 apt -yy -qq upgrade > /dev/null
-apt -yy install ${PACKAGES//\\n/ } --no-install-recommends 
+apt -yy install ${PACKAGES//\\n/ }
 
 
 # -- Add znx.
